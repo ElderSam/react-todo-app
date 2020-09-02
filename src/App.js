63a09ac1c1de707c53da2e764afcf08130651341
ElderSam <eldersamuel98@gmail.com>
2020-09-02 11:33:51 -0300

    add useState for array todos and pass param to TodoList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodoList';
 
 function App() {
+
+  const [ todos, setTodos] = useState(['todo1', 'todo2', 'todo3']);
+
   return (
     <>
-      <TodoList />
+      <TodoList todos={todos} />
       <input type="text" />
       <button>Add Todo</button>
       <button>Clear Completed</button>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export default function TodoList(){
+export default function TodoList({ todos }){
     return (
-        <div>Todo List</div>
+        <div>
+            {todos.length}
+        </div>
     );
 }
