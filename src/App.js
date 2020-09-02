dcc372547bedc907663efd5acfdf131367350ec0
ElderSam <eldersamuel98@gmail.com>
2020-09-02 13:48:47 -0300

    adding new todos in TodoList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList';
+import uuidv4 from 'uuid/v4'; //to create randomic id
 
 function App() {
 
-  const [ todos, setTodos] = useState([{id: 1, name: 'todo1', completed: true}, {id: 2, name: 'todo2', completed: false}]);
+  const [ todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+
+    //console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, completed: false}]
+    })
+
+    todoNameRef.current.value = null //empties the input value
+  }
 
   return (
     <>
       <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed</button>
       <div>0 left to do</div>
     </> 
