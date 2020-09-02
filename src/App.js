4d5238a42f7c6659985cbd601f968a310e2abe8d
ElderSam <eldersamuel98@gmail.com>
2020-09-02 15:03:47 -0300

    using LOCAL_STORAGE, complete todos and clear TodoList

--- src/App.js
+++ src/App.js
@@ -18,16 +18,28 @@ function App() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
 
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
 
     //console.log(name)
     setTodos(prevTodos => {
-      return [...prevTodos, {id: uuid(), name: name, completed: false}]
+      return [...prevTodos, {id: uuid(), name: name, complete: false}]
     })
 
     todoNameRef.current.value = null //empties the input value
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
   }
 
   return (
@@ -35,7 +47,7 @@ function App() {
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button onClick={handleClearTodos}>Clear Completed</button>
+      <button onClick={handleClearTodos}>Clear Complete</button>
       <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </> 
   )
--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, toggleTodo }) {
+    
+    function handleTodoClick() {
+        toggleTodo(todo.id)
+    }
+
     return (
         <div>
-            <input type="checkbox" checked={todo.completed} />
+            <input type="checkbox" checked={todo.complete} onChange={handleTodoClick} />
             <label>
                 {todo.name}
             </label>
